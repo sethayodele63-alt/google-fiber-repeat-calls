57eb3d9a90f35dca89974af5020b685f29770248
sethayodele63-alt <sethayodele63@gmail.com>
2026-04-25 11:04:26 +0100

    Initial commit: Google Fiber repeat calls project

---
+++ SQL Query for Table merging.sql
@@ -0,0 +1,8 @@
+SELECT*
+FROM `continual-works-490313-h3.490313.Fiber`
+UNION ALL
+SELECT*
+FROM `continual-works-490313-h3.490313.Fiber 2`
+UNION ALL
+SELECT*
+FROM `continual-works-490313-h3.490313.fiber 3`
